469c6bfeef9d3d5cce3d4277f7344c7f9daf8109
Bhargavi Gohil <bhargavigohil098@gmail.com>
2026-09-19 10:46:05 +0530

    Refactor findMedianSortedArrays for efficiency

--- ASSIGNMENT-2/Q5.java
+++ ASSIGNMENT-2/Q5.java
@@ -1,33 +1,34 @@
-public class Q5 {
-    public double findMedianSortedArrays(int[] nums1,int[] nums2) {
-        int[] arr=new int[nums1.length+nums2.length];
-
-        int k=0;
-
-        for(int i=0;i<nums1.length;i++) {
-            arr[k]=nums1[i];
-            k++;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-        for(int i=0;i<nums2.length;i++) {
-            arr[k]=nums2[i];
-            k++;
-        }
-        for(int i=0;i<arr.length-1;i++) {
-            for(int j=i+1;j<arr.length;j++) {
-                if(arr[i]>arr[j]) {
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = 0, right = m;
+        while (left <= right) {
+            int mid1 = left + (right - left) / 2;
+            int mid2 = (m + n + 1) / 2 - mid1;
+
+            int maxLeft1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int minRight1 = (mid1 == m) ? Integer.MAX_VALUE : nums1[mid1];
+
+            int maxLeft2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
+            int minRight2 = (mid2 == n) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                // Correct partition found
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return Math.max(maxLeft1, maxLeft2);
                 }
+            } else if (maxLeft1 > minRight2) {
+                right = mid1 - 1; // move left
+            } else {
+                left = mid1 + 1; // move right
             }
         }
-
-        int n=arr.length;
-
-        if(n%2==1) {
-            return arr[n/2];
-        }
-
-        return (arr[n/2-1]+arr[n/2])/2.0;
+       throw new IllegalArgumentException("Input arrays are not sorted.");
     }
 }
